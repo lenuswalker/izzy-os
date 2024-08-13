76042a914915bb006f2d8722c509d3fc6591a956
Lenus Walker <lenusiwalker@outlook.com>
2024-08-13 10:49:18 -0400

    format files and make changes to gschema overrides

--- files/scripts/izzy-os-changes.sh
+++ files/scripts/izzy-os-changes.sh
@@ -5,6 +5,9 @@ set -ouex pipefail
 if [[ -f /usr/share/applications/gnome-system-monitor.desktop ]]; then
     sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/gnome-system-monitor.desktop
 fi
+if [[ -f /usr/share/applications/gnome-system-monitor-kde.desktop ]]; then
+    sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/gnome-system-monitor-kde.desktop
+fi
 if [[ -f /usr/share/applications/org.gnome.SystemMonitor.desktop ]]; then
     sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/org.gnome.SystemMonitor.desktop
 fi

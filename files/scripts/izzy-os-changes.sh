9bf570d7ee43d3bb4a76db71f0b22d0075b35cfa
Lenus Walker <walker.lenus@gmail.com>
2024-08-12 20:40:06 -0400

    Added more changes to fix bug and include system76-power

--- files/scripts/izzy-os-changes.sh
+++ files/scripts/izzy-os-changes.sh
@@ -2,12 +2,10 @@
 
 set -ouex pipefail
 
-if [[ "${BASE_IMAGE_NAME}" = "silverblue" ]]; then
-    if [[ -f /usr/share/applications/gnome-system-monitor.desktop ]]; then
-        sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/gnome-system-monitor.desktop
-    fi
-    if [[ -f /usr/share/applications/org.gnome.SystemMonitor.desktop ]]; then
-        sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/org.gnome.SystemMonitor.desktop
-    fi
-    #sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/org.gnome.Terminal.desktop
+if [[ -f /usr/share/applications/gnome-system-monitor.desktop ]]; then
+    sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/gnome-system-monitor.desktop
 fi
+if [[ -f /usr/share/applications/org.gnome.SystemMonitor.desktop ]]; then
+    sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/org.gnome.SystemMonitor.desktop
+fi
+#sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/org.gnome.Terminal.desktop

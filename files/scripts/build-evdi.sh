a323cce3c1c5115906b7d401854b230155e44ea4
Lenus Walker <walker.lenus@gmail.com>
2024-08-15 22:16:57 -0400

    Update to remove unnecessary stuff and added some packages

--- files/scripts/build-evdi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ouex pipefail
-
-/tmp/files/scripts/kernel-modules.sh build
-/tmp/files/scripts/kernel-modules.sh load
--- files/scripts/install-displaylink-evdi.sh
+++ files/scripts/install-displaylink-evdi.sh
@@ -3,5 +3,3 @@
 set -ouex pipefail
 
 /tmp/files/scripts/github-release-install.sh displaylink-rpm/displaylink-rpm x86_64 fedora-$(rpm -E %fedora)
-
-rpm-ostree uninstall make gcc glibc-devel openssl-devel zlib-ng-compat-devel libxcrypt-devel libzstd-devel perl-base

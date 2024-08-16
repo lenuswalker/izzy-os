87dcbffcfda3562ca1063a8f7981067639ad6480
Lenus Walker <walker.lenus@gmail.com>
2024-08-15 21:20:00 -0400

    remove some build packages

--- files/scripts/install-displaylink-evdi.sh
+++ files/scripts/install-displaylink-evdi.sh
@@ -3,3 +3,5 @@
 set -ouex pipefail
 
 /tmp/files/scripts/github-release-install.sh displaylink-rpm/displaylink-rpm x86_64 fedora-$(rpm -E %fedora)
+
+rpm-ostree uninstall rpm-build make gcc glibc-devel openssl-devel zlib-ng-compat-devel libxcrypt-devel libzstd-devel perl-base

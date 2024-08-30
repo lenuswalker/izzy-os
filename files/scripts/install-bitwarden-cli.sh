734dafc82673be6ac566034e10298bebed28f905
Lenus Walker <lenusiwalker@outlook.com>
2024-08-30 09:41:07 -0400

    attempt to install bw-cli

---
+++ files/scripts/install-bitwarden-cli.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+wget "https://vault.bitwarden.com/download/?app=cli&platform=linux" -O /tmp/bitwarden.zip
+
+unzip /tmp/bitwarden.zip -d /tmp
+
+chmod u+x /tmp/bw
+
+mw /tmp/bw /usr/local/bw

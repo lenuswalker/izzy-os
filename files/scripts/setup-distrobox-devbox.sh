233a0f45a7c023517e1eec3373e5b6b68a5b0d04
Lenus Walker <lenusiwalker@outlook.com>
2024-09-01 21:28:21 -0400

    update to distrobox script

--- files/scripts/setup-distrobox-devbox.sh
+++ files/scripts/setup-distrobox-devbox.sh
@@ -3,17 +3,17 @@
 set -ouex pipefail
 
 # Create devbox
-distrobox create --name devbox --image ubuntu:latest --home $HOME/distrobox/home/devbox
+distrobox create --name devbox --image ubuntu:latest --yes --home $HOME/distrobox/home/devbox
 distrobox enter devbox
 
 # Update distrobox
-sudo apt update && sudo apt upgrade
-sudo apt install software-properties-common gnome-keyring nano
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y software-properties-common gnome-keyring nano
 
 # Install fastfetch
 sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
 sudo apt update
-sudo apt install fastfetch
+sudo apt install -y fastfetch
 
 # Install nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
@@ -30,7 +30,7 @@ sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
 sudo apt update
-sudo apt install apt-transport-https code
+sudo apt install -y apt-transport-https code
 distrobox-export --app code
 
 # Install dotnet

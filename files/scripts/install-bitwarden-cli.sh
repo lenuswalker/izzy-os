bed70fb51a9952cae7516c0cc1ce0285a4e4c83d
Lenus Walker <walker.lenus@gmail.com>
2024-09-04 21:15:58 -0400

    update bitwarden install script

--- files/scripts/install-bitwarden-cli.sh
+++ files/scripts/install-bitwarden-cli.sh
@@ -5,4 +5,7 @@ set -ouex pipefail
 wget "https://vault.bitwarden.com/download/?app=cli&platform=linux" -O /tmp/bitwarden.zip
 unzip /tmp/bitwarden.zip -d /tmp
 chmod u+x /tmp/bw
+if [ ! -d /usr/local/bin ]; then
+    mkdir -p /usr/local/bin
+fi
 mv /tmp/bw /usr/local/bin/bw
